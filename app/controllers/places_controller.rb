05f344c397c610d3a5cf698980167807c15aaa99
Chris Marasco <cxm6467@gmail.com>
2017-08-09 21:27:14 +0000

    Show method working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,21 @@
 class PlacesController < ApplicationController
-
-  ## Make sure a user is logged in first
   before_action :authenticate_user!, only: [:new, :create]
 
-  ## An action for the index view
   def index
-    @places = Place.paginate(:page => params[:page], :per_page => 3)
+    @places = Place.all
   end
 
-  ## Action to controller creation of new places
   def new
     @place = Place.new
   end
 
-  ##
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
@@ -24,5 +23,4 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 end

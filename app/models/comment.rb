56a2476e2bc3fa27f5e19d058a13ed0237c4574a
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-15 18:36:52 +0000

    Comment rating drop down

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {,
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three_stars': '3_stars',
+    'four_stars': '4_stars',
+    'five_stars': '5_stars'
+  }
+
 end

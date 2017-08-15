6260acd0f7579b136a3905265a7eec7a09e3f801
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-15 18:57:45 +0000

    Comment rating drop down -- The right way

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,9 +5,12 @@ class Comment < ApplicationRecord
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_stars',
-    'three_stars': '3_stars',
-    'four_stars': '4_stars',
-    'five_stars': '5_stars'
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
   }
 
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end

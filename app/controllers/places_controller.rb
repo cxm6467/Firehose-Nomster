705880b142f3d121061c9a11c41b362c4f464e64
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-17 05:57:55 +0000

    It works locally AND globally?

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,7 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 3)
+    @place = Place.find(params[:id])
   end
 
   def new

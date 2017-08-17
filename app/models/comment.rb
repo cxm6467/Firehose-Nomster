6f7eefa2cecac880d0d41bf61e559a24fa83d1ea
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-17 19:07:14 +0000

    E-mails after comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
 
+  after_create :send_comment_email
+
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_stars',
@@ -13,4 +15,9 @@ class Comment < ApplicationRecord
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver
+  end
+
 end
